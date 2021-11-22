a0c22f67f50387381122ef0034fe02a44b0dbcdd
Shira Laury <shira.laury@gmail.com>
2021-11-22 17:08:16 +0200

    Trying to pull...

--- UIL/UIL/Program.cs
+++ UIL/UIL/Program.cs
@@ -17,6 +17,8 @@ namespace UIL
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            
         }
     }
 }
