afa425676a961ba15ee6655cae7266ded06c3e18
Chani Wachsstock <chaniwachsstock@gmail.com>
2021-12-06 12:40:21 +0200

    BLL test

--- BLL/BLLTest.cs
+++ BLL/BLLTest.cs
@@ -13,38 +13,11 @@ namespace BLL
         {
 
 
-            //ProductBLL obj = new ProductBLL();
-            ////Shira Laury
-            //obj.Delete(78889);
-
-            //try
-            //{
-
-            //    obj.Delete(78889);
-            //    obj.Update(98342, "fishing rod", (decimal)45.65, 32);
-            //    obj.ReadAll();
-            //    obj.Read(19238);
-            //}
-            //catch(ProductNumberNotFound numOut)
-            //{
-            //    Console.WriteLine(numOut.Message); 
-
-            //}
-            ////catch()
-
-
-            ////Chani Wachsstock
-            //try
-            //{
-            //    ProductBLL products = new ProductBLL();
-            //    products.Create(19387, "ballie", (decimal)32.00, 98);
-            //}
-            //catch (Exception)
-            //{
-            //    Console.WriteLine("Oops... That product already exists");
-            //}
+           
             CustomerBLL mister = new CustomerBLL();
-            mister
+            mister.Create("joe", 111, 1231232312, 2021, 12);
+
+            Console.WriteLine(mister.Read(11111));
         }
     }
 }
