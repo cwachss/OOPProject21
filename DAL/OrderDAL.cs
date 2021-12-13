2b3b4be8caaa8cc0e189bce5917b0034392ba30e
Chani Wachsstock <chaniwachsstock@gmail.com>
2021-12-13 16:26:53 +0200

    Create, Update, Delete written but not tested

--- DAL/OrderDAL.cs
+++ DAL/OrderDAL.cs
@@ -13,13 +13,36 @@ namespace DAL
         int orderNumber = 100;
 
 
-        public void AddOrder(int customerID, int productID)
+        public void Create(int customerID, int productID)
         {
             Order order = new Order(orderNumber, customerID, productID);
             orderList.Add(order);
             orderNumber += 100;
         }
 
+        public void Delete(int anOrderNumber)
+        {
+            for (int i = 0; i < orderList.Count; i++) //I feel like I made this method too unwieldy
+            {
+                if (anOrderNumber == orderList[i].OrderNumber)//checks if we've found the sought-for product number
+                {
+
+                    orderList.Remove(orderList[i]);
+                    break;
+                }
+                if (i == orderList.Count)//if never found product number, it throws an exception.
+                {
+                    throw new ProductNumberNotFound();
+                }
+            }
+        }
+
+        public void Update(int anOrderNumber, int customerID, int productID)
+        {
+            Delete(anOrderNumber);
+            Order order = new Order(anOrderNumber, customerID, productID);
+            orderList.Add(order);
+        }
 
     }
 }
