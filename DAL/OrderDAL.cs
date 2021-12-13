dc13ecd0abacf8b7be1ec5d43a2b32e85b1986b6
Chani Wachsstock <chaniwachsstock@gmail.com>
2021-12-13 16:17:22 +0200

    OrderDAL initiated

---
+++ DAL/OrderDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace DAL
+{
+    class OrderDAL
+    {
+        List<Order> orderList = new List<Order> ();
+        int orderNumber = 100;
+
+
+        public void AddOrder(int customerID, int productID)
+        {
+            Order order = new Order(orderNumber, customerID, productID);
+            orderList.Add(order);
+            orderNumber += 100;
+        }
+
+
+    }
+}
