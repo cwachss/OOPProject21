3caa4e2f2dd4f0a89b6f8b225b406a0f33968e13
Chani Wachsstock <chaniwachsstock@gmail.com>
2022-01-05 14:58:24 +0200

    minor change

--- UserInterface2.0/MainMenu.Designer.cs
+++ UserInterface2.0/MainMenu.Designer.cs
@@ -39,12 +39,12 @@ namespace UserInterface2._0
             this.pictureBoxProducts = new System.Windows.Forms.PictureBox();
             this.pictureBoxCustomer = new System.Windows.Forms.PictureBox();
             this.groupBoxSignIn = new System.Windows.Forms.GroupBox();
-            this.labelEmployeeName = new System.Windows.Forms.Label();
-            this.labelID = new System.Windows.Forms.Label();
-            this.labelPassword = new System.Windows.Forms.Label();
-            this.textBoxID = new System.Windows.Forms.TextBox();
-            this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.buttonSignIn = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelEmployeeName = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrders)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProducts)).BeginInit();
@@ -56,10 +56,10 @@ namespace UserInterface2._0
             // 
             this.labelWelcome.AutoSize = true;
             this.labelWelcome.Font = new System.Drawing.Font("Agency FB", 27.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelWelcome.Location = new System.Drawing.Point(144, 86);
+            this.labelWelcome.Location = new System.Drawing.Point(108, 70);
             this.labelWelcome.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.labelWelcome.Name = "labelWelcome";
-            this.labelWelcome.Size = new System.Drawing.Size(473, 57);
+            this.labelWelcome.Size = new System.Drawing.Size(379, 45);
             this.labelWelcome.TabIndex = 7;
             this.labelWelcome.Text = "Welcome to Toys of All Sorts";
             // 
@@ -68,10 +68,10 @@ namespace UserInterface2._0
             this.labelProducts.AutoSize = true;
             this.labelProducts.BackColor = System.Drawing.Color.Transparent;
             this.labelProducts.Font = new System.Drawing.Font("Agency FB", 27.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelProducts.Location = new System.Drawing.Point(101, 485);
+            this.labelProducts.Location = new System.Drawing.Point(76, 394);
             this.labelProducts.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.labelProducts.Name = "labelProducts";
-            this.labelProducts.Size = new System.Drawing.Size(153, 55);
+            this.labelProducts.Size = new System.Drawing.Size(120, 46);
             this.labelProducts.TabIndex = 8;
             this.labelProducts.Text = "Products";
             this.labelProducts.Visible = false;
@@ -81,10 +81,10 @@ namespace UserInterface2._0
             this.labelCustomers.AutoSize = true;
             this.labelCustomers.BackColor = System.Drawing.Color.White;
             this.labelCustomers.Font = new System.Drawing.Font("Agency FB", 27.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCustomers.Location = new System.Drawing.Point(463, 485);
+            this.labelCustomers.Location = new System.Drawing.Point(347, 394);
             this.labelCustomers.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.labelCustomers.Name = "labelCustomers";
-            this.labelCustomers.Size = new System.Drawing.Size(180, 55);
+            this.labelCustomers.Size = new System.Drawing.Size(140, 46);
             this.labelCustomers.TabIndex = 9;
             this.labelCustomers.Text = "Customers";
             this.labelCustomers.Visible = false;
@@ -94,10 +94,10 @@ namespace UserInterface2._0
             this.labelOrders.AutoSize = true;
             this.labelOrders.BackColor = System.Drawing.Color.White;
             this.labelOrders.Font = new System.Drawing.Font("Agency FB", 27.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelOrders.Location = new System.Drawing.Point(877, 485);
+            this.labelOrders.Location = new System.Drawing.Point(658, 394);
             this.labelOrders.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.labelOrders.Name = "labelOrders";
-            this.labelOrders.Size = new System.Drawing.Size(125, 55);
+            this.labelOrders.Size = new System.Drawing.Size(98, 46);
             this.labelOrders.TabIndex = 11;
             this.labelOrders.Text = "Orders";
             this.labelOrders.Visible = false;
@@ -105,10 +105,9 @@ namespace UserInterface2._0
             // pictureBoxOrders
             // 
             this.pictureBoxOrders.Image = global::UserInterface2._0.Properties.Resources.shoppingcart1;
-            this.pictureBoxOrders.Location = new System.Drawing.Point(751, 167);
-            this.pictureBoxOrders.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxOrders.Location = new System.Drawing.Point(563, 136);
             this.pictureBoxOrders.Name = "pictureBoxOrders";
-            this.pictureBoxOrders.Size = new System.Drawing.Size(377, 340);
+            this.pictureBoxOrders.Size = new System.Drawing.Size(283, 276);
             this.pictureBoxOrders.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBoxOrders.TabIndex = 10;
             this.pictureBoxOrders.TabStop = false;
@@ -118,10 +117,10 @@ namespace UserInterface2._0
             // pictureBoxLogo
             // 
             this.pictureBoxLogo.Image = global::UserInterface2._0.Properties.Resources.toysLogo3;
-            this.pictureBoxLogo.Location = new System.Drawing.Point(9, 9);
+            this.pictureBoxLogo.Location = new System.Drawing.Point(7, 7);
             this.pictureBoxLogo.Margin = new System.Windows.Forms.Padding(1);
             this.pictureBoxLogo.Name = "pictureBoxLogo";
-            this.pictureBoxLogo.Size = new System.Drawing.Size(120, 139);
+            this.pictureBoxLogo.Size = new System.Drawing.Size(90, 113);
             this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxLogo.TabIndex = 6;
             this.pictureBoxLogo.TabStop = false;
@@ -130,10 +129,9 @@ namespace UserInterface2._0
             // pictureBoxProducts
             // 
             this.pictureBoxProducts.Image = global::UserInterface2._0.Properties.Resources.teddy3_02;
-            this.pictureBoxProducts.Location = new System.Drawing.Point(-3, 167);
-            this.pictureBoxProducts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxProducts.Location = new System.Drawing.Point(-2, 136);
             this.pictureBoxProducts.Name = "pictureBoxProducts";
-            this.pictureBoxProducts.Size = new System.Drawing.Size(360, 340);
+            this.pictureBoxProducts.Size = new System.Drawing.Size(270, 276);
             this.pictureBoxProducts.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxProducts.TabIndex = 3;
             this.pictureBoxProducts.TabStop = false;
@@ -143,10 +141,9 @@ namespace UserInterface2._0
             // pictureBoxCustomer
             // 
             this.pictureBoxCustomer.Image = global::UserInterface2._0.Properties.Resources.toystorepeoples;
-            this.pictureBoxCustomer.Location = new System.Drawing.Point(365, 167);
-            this.pictureBoxCustomer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBoxCustomer.Location = new System.Drawing.Point(274, 136);
             this.pictureBoxCustomer.Name = "pictureBoxCustomer";
-            this.pictureBoxCustomer.Size = new System.Drawing.Size(377, 340);
+            this.pictureBoxCustomer.Size = new System.Drawing.Size(283, 276);
             this.pictureBoxCustomer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxCustomer.TabIndex = 2;
             this.pictureBoxCustomer.TabStop = false;
@@ -161,72 +158,80 @@ namespace UserInterface2._0
             this.groupBoxSignIn.Controls.Add(this.labelPassword);
             this.groupBoxSignIn.Controls.Add(this.labelID);
             this.groupBoxSignIn.Controls.Add(this.labelEmployeeName);
-            this.groupBoxSignIn.Location = new System.Drawing.Point(286, 162);
+            this.groupBoxSignIn.Location = new System.Drawing.Point(293, 136);
+            this.groupBoxSignIn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBoxSignIn.Name = "groupBoxSignIn";
-            this.groupBoxSignIn.Size = new System.Drawing.Size(258, 213);
+            this.groupBoxSignIn.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBoxSignIn.Size = new System.Drawing.Size(248, 192);
             this.groupBoxSignIn.TabIndex = 12;
             this.groupBoxSignIn.TabStop = false;
             this.groupBoxSignIn.Text = "Sign In";
             // 
+            // buttonSignIn
+            // 
+            this.buttonSignIn.Location = new System.Drawing.Point(23, 153);
+            this.buttonSignIn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSignIn.Name = "buttonSignIn";
+            this.buttonSignIn.Size = new System.Drawing.Size(64, 19);
+            this.buttonSignIn.TabIndex = 5;
+            this.buttonSignIn.Text = "Enter";
+            this.buttonSignIn.UseVisualStyleBackColor = true;
+            this.buttonSignIn.Click += new System.EventHandler(this.buttonSignIn_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(23, 105);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(152, 20);
+            this.textBoxPassword.TabIndex = 4;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(23, 53);
+            this.textBoxID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(152, 20);
+            this.textBoxID.TabIndex = 3;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(21, 127);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Password";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(21, 75);
+            this.labelID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(67, 13);
+            this.labelID.TabIndex = 1;
+            this.labelID.Text = "Employee ID";
+            // 
             // labelEmployeeName
             // 
             this.labelEmployeeName.AutoSize = true;
-            this.labelEmployeeName.Location = new System.Drawing.Point(28, 32);
+            this.labelEmployeeName.Location = new System.Drawing.Point(21, 26);
+            this.labelEmployeeName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelEmployeeName.Name = "labelEmployeeName";
             this.labelEmployeeName.Size = new System.Drawing.Size(196, 13);
             this.labelEmployeeName.TabIndex = 0;
             this.labelEmployeeName.Text = "Please enter your user ID and Password";
             this.labelEmployeeName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // labelID
-            // 
-            this.labelID.AutoSize = true;
-            this.labelID.Location = new System.Drawing.Point(28, 88);
-            this.labelID.Name = "labelID";
-            this.labelID.Size = new System.Drawing.Size(67, 13);
-            this.labelID.TabIndex = 1;
-            this.labelID.Text = "Employee ID";
-            // 
-            // labelPassword
-            // 
-            this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(28, 137);
-            this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(53, 13);
-            this.labelPassword.TabIndex = 2;
-            this.labelPassword.Text = "Password";
-            // 
-            // textBoxID
-            // 
-            this.textBoxID.Location = new System.Drawing.Point(31, 65);
-            this.textBoxID.Name = "textBoxID";
-            this.textBoxID.Size = new System.Drawing.Size(201, 20);
-            this.textBoxID.TabIndex = 3;
-            // 
-            // textBoxPassword
-            // 
-            this.textBoxPassword.Location = new System.Drawing.Point(31, 114);
-            this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(201, 20);
-            this.textBoxPassword.TabIndex = 4;
-            this.textBoxPassword.UseSystemPasswordChar = true;
-            // 
-            // buttonSignIn
-            // 
-            this.buttonSignIn.Location = new System.Drawing.Point(31, 168);
-            this.buttonSignIn.Name = "buttonSignIn";
-            this.buttonSignIn.Size = new System.Drawing.Size(86, 23);
-            this.buttonSignIn.TabIndex = 5;
-            this.buttonSignIn.Text = "Enter";
-            this.buttonSignIn.UseVisualStyleBackColor = true;
-            this.buttonSignIn.Click += new System.EventHandler(this.buttonSignIn_Click);
-            // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(847, 458);
+            this.ClientSize = new System.Drawing.Size(847, 451);
             this.Controls.Add(this.groupBoxSignIn);
             this.Controls.Add(this.labelOrders);
             this.Controls.Add(this.pictureBoxOrders);
@@ -237,7 +242,6 @@ namespace UserInterface2._0
             this.Controls.Add(this.pictureBoxProducts);
             this.Controls.Add(this.pictureBoxCustomer);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainMenu";
