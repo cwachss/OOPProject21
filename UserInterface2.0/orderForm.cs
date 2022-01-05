6e868493b2709deee021b1717a73306bd95edc0d
Shira Laury <shira.laury@gmail.com>
2022-01-05 15:09:26 +0200

    n/t imp

--- UserInterface2.0/orderForm.cs
+++ UserInterface2.0/orderForm.cs
@@ -98,14 +98,14 @@ namespace UserInterface2._0
                 textBoxCustomerID.Clear();
                 textBoxProductNumber.Clear();
             }
-            //catch (ProductNumberNotFound)
-            //{
-            //    MessageBox.Show("Product Not Found");
-            //}
-            //catch (CustomerNotFound)
-            //{
-            //    MessageBox.Show("Customer not found");
-            //}
+            catch (ProductNumberNotFound)
+            {
+                MessageBox.Show("Product Not Found");
+            }
+            catch (CustomerNotFound)
+            {
+                MessageBox.Show("Customer not found");
+            }
             catch
             {
                 MessageBox.Show("Please fill in all textboxes");
